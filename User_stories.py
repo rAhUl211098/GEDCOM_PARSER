8d1d9c3c7f87409b8872a33194e29b637fa865b9
Monarch Parmar <42401530+parmarmonarch@users.noreply.github.com>
2022-12-17 06:41:08 -0500

    Update User_stories.py
    Adding user stories 1 and 2

--- User_stories.py
+++ User_stories.py
@@ -63,3 +63,36 @@ def us4_next_dob ( obj , pt = False , debug = False , write = False ) :
     if write :
         birthday_header = "US4: Next Birthdays:"
         obj.updated_file.append ( [ birthday_header , dob_tbl ] )
+        
+# To check less than 6 children are born at a single time
+def us1_birth_less_than_6 ( obj , debug = False ) :
+
+    fam_lst = [ ]
+    for fam in obj.families.values ( ) :
+        dob_list = [ ]
+        dob_check = [ ]
+        for c in fam.child_lst :
+            for individual in obj.individuals.values ( ) :
+                if individual.indi_id == c :
+                    dob_list.append ( individual.dob )
+
+        for bday in dob_list :
+            if dob_list.count ( bday ) > 5 and dob_check.count ( bday ) == 0 :
+                dob_check.append ( bday )
+                fam_lst.append ( fam.fam_id )
+                obj.logger ( "ERROR" , "FAMILY" , "US1" , fam.l_num [ "CHIL" ] [ 0 ] [ 1 ] , fam.fam_id ,
+                              f"{dob_list.count ( bday )} childs born on the same date" )
+
+    if debug :
+        return fam_lst   
+# To check there are less than 15 children
+def us2_less_than_15_siblings ( obj , debug = False ) :
+
+    family_list = [ ]
+    for family in obj.families.values ( ) :
+        if len ( family.child_lst ) >= 15 :
+            family_list.append ( family.fam_id )
+            obj.logger ( "ERROR" , "FAMILY" , "US2" , family.l_num [ "CHIL" ] [ 0 ] [ 1 ] , family.fam_id ,
+                          f"Fam has {len ( family.child_lst )} childs" )
+    if debug :
+        return family_list
