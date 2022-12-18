761e3a66b1787f6d4e46954da5bd53665df7f9f0
rahul <rahul.chandani2110@gmail.com>
2022-12-18 02:47:40 -0500

    Adding unittest_sprint1.py for automated tests. And adding more test cases

--- unittest_sprint1.py
+++ unittest_sprint1.py
@@ -1,28 +1,37 @@
 import unittest
 from User_stories import *
 
-def us1_birth_less_than_6_test(self):
-    obj = GedcomParser(r'./data/test_data.ged', pt=False)
-    families = obj.us1_birth_less_than_6(debug=True)
-    self.assertIn('@F6@', families)
-    self.assertEqual(len(families), 1)
 
-def us2_less_than_15_siblings_test(self):
-    obj = GedcomParser(r'./data/test_data.ged', pt=False)
-    families = obj.us2_less_than_15_siblings(debug=True)
-    self.assertIn('@F6@', families)
-    self.assertEqual(len(families), 1)
+class Sprint1_Test(unittest.TestCase):
 
-def us3_orphans_list_test(self):
-    obj = GedcomParser(r'./data/test_data.ged', pt=False)
-    orphans = obj.us3_orphans_list(debug=True)
-    self.assertTrue(len(orphans) == 1)
-    self.assertFalse(len(orphans) == 0)
 
-def us4_next_dob_test( self ):
-    obj = GedcomParser(r'./data/test_data.ged', pt=False)
-    debug_list = obj.us4_next_dob(pt=False, debug=True)
-    for time_delta in debug_list:
-        self.assertTrue(time_delta.days < 30)
+    def test_us1_birth_less_than_6( self ):
+        obj = GedcomParser(r'./data/test_data.ged', pt=False)
+        families = us1_birth_less_than_6(obj,debug=True)
+        self.assertTrue('@F6@',families)
+        self.assertIn('@F6@', families)
+        self.assertEqual(len(families), 1)
 
-unittest.main(exit=False, verbosity=2)
+    def test_us2_less_than_15_siblings(self):
+        obj = GedcomParser(r'./data/test_data.ged', pt=False)
+        families = us2_less_than_15_siblings(obj,debug=True)
+        self.assertIn('@F6@', families)
+        self.assertEqual(len(families), 1)
+
+    def test_us3_orphans_list(self):
+        obj = GedcomParser(r'./data/test_data.ged', pt=False)
+        orphans = us3_orphans_list(obj,debug=True)
+        self.assertTrue(len(orphans) == 1)
+        self.assertFalse(len(orphans) == 0)
+
+    def test_us4_next_dob( self ):
+        obj = GedcomParser(r'./data/test_data.ged', pt=False)
+        debug_list = us4_next_dob(obj,pt=False, debug=True)
+        for time_delta in debug_list:
+            self.assertTrue(time_delta.days < 30)
+        for time_delta in debug_list:
+            self.assertFalse(time_delta.days >= 30)
+
+if __name__ == '__main__':
+
+    unittest.main(exit=False, verbosity=2)
