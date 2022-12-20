7d2aecba07e2d8c4fe75042aa7153a18345737ee
Monarch Parmar <42401530+parmarmonarch@users.noreply.github.com>
2022-12-19 20:15:53 -0500

    Update user_stories_sprint4.py
    Adding user stories 13 and 14

--- user_stories_sprint4.py
+++ user_stories_sprint4.py
@@ -56,3 +56,40 @@ def us16_check_divorce_while_alive ( obj  , pt = True , debug = False ) :
 
     if debug :
         return debugger
+    
+# To check and avoid incest in a family
+def us14_check_married_to_child ( obj , pt = False , debug = False ) :
+    discrepancy = [ ]
+
+    for fam in obj.families.values ( ) :
+        for c in fam.child_lst :
+            if str ( fam.wife ) or str ( fam.husband ) == c :
+                obj.logger ( "ERROR" , "FAMILY" , "US14" , fam.l_num [ "FAM" ] , fam.fam_id ,
+                              f"Marriage between parent and child." )
+                discrepancy.append ( fam.fam_id )
+
+    if debug :
+        return discrepancy
+# To check the genders assigned to husbands and wives
+def us13_check_gender_of_spouse ( obj , pt = False , debug = False ) :
+    discrepancy = [ ]
+
+    for fam in obj.families.values ( ) :
+        for ind in obj.individuals.values ( ) :
+            if ind.indi_id == fam.husband :
+                if ind.sex != "M" :
+                    obj.logger ( "ERROR" , "FAMILY" , "US13" , fam.l_num [ "HUSB" ] , fam.fam_id ,
+                                  f"Husband with id {ind.indi_id} and name {ind.name} is not male." )
+                    discrepancy.append ( ind.indi_id )
+
+                elif ind.indi_id == fam.wife and ind.sex != "F" :
+                    obj.logger ( "ERROR" , "FAMILY" , "US13" , fam.l_num [ "WIFE" ] , fam.fam_id ,
+                                  f"Wife with id {ind.indi_id} and name {ind.name} is not female." )
+                    discrepancy.append ( ind.indi_id )
+            elif ind.indi_id == fam.wife and ind.sex != "F" :
+                obj.logger ( "ERROR" , "FAMILY" , "US13" , fam.l_num [ "WIFE" ] , fam.fam_id ,
+                              f"Wife with id {ind.indi_id} and name {ind.name} is not female." )
+                discrepancy.append ( ind.indi_id )
+
+    if debug :
+        return discrepancy    
