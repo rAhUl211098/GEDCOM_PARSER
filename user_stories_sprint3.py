d7b6d98dc732e994fc0d11a6378481e22daf381d
Monarch Parmar <42401530+parmarmonarch@users.noreply.github.com>
2022-12-19 19:26:57 -0500

    Update user_stories_sprint3.py
    Adding user stories 9 and 10

--- user_stories_sprint3.py
+++ user_stories_sprint3.py
@@ -47,3 +47,62 @@ def us12_deaths_in_last_month ( obj , pt = False , debug = False , write = False
     if write :
         head = "US12:Died Recently :"
         obj.updated_file.append ( [ head , table_of_deaths_in_lastmonth ] )
+        
+#To check if children are born before or after marriage of their parents
+def us9_children_pre_marriage ( obj , debug = False ) :
+
+    debugger = [ ]
+    for fam in obj.families.values ( ) :
+        if fam.date_of_wedding :
+            childs = [ ]
+
+            for ind in obj.individuals.values ( ) :
+                for childs in fam.child_lst :
+                    if ind.indi_id == childs :
+                        childs.append ( ind )
+
+            for child in childs :
+                if child.dob < fam.date_of_wedding :
+                    obj.logger ( "ANOMALY" , "FAMILY" , "US08" , fam.l_num [ "CHIL" ] [ 0 ] [ 1 ] , fam.fam_id ,
+                                  f"Child with id {child.indi_id} born {child.dob.strftime ( GedcomParser.dateFormatter )} before marriage of parents on {fam.date_of_wedding.strftime ( GedcomParser.dateFormatter )}" )
+                    debugger.append ( child.indi_id )
+                if fam.divorced and child.dob > (fam.date_of_divorce + datetime.timedelta ( 9 * 365 / 12 )) :
+                    obj.logger ( "ANOMALY" , "FAMILY" , "US08" , fam.l_num [ "CHIL" ] [ 0 ] [ 1 ] , fam.fam_id ,
+                                  f"Child with id {child.indi_id} born {child.dob.strftime ( GedcomParser.dateFormatter )} after the divorce of parents on {fam.date_of_divorce.strftime ( GedcomParser.dateFormatter )}" )
+                    debugger.append ( child.indi_id )
+
+    if debug :
+        return debugger
+# To chek if children are born before death of mother or after 9 months of death of the father
+def us10_child_before_parents_death ( obj , debug = False ) :
+
+    debugger = [ ]
+    for fam in obj.families.values ( ) :
+        if fam.husband and fam.wife and len ( fam.child_lst ) != 0 :
+            childs = [ ]
+
+            for ind in obj.individuals.values ( ) :
+                if ind.indi_id == fam.husband :
+                    husband = ind
+
+                if ind.indi_id == fam.wife :
+                    wife = ind
+
+                for c in fam.child_lst :
+                    if ind.indi_id == c :
+                        childs.append ( ind )
+
+            for c in childs :
+                if wife.death_date and c.dob > wife.death_date :
+                    obj.logger ( "ANOMALY" , "FAMILY" , "US10" , fam.l_num [ "CHIL" ] [ 0 ] [ 1 ] , fam.fam_id ,
+                                  f"Child with id {c.indi_id} born {c.dob.strftime ( GedcomParser.dateFormatter )} after mother's death on date: {wife.death_date.strftime ( GedcomParser.dateFormatter )}" )
+                    debugger.append ( c.indi_id )
+
+                if husband.death_date and c.dob > (husband.death_date + datetime.timedelta ( 9 * 365 / 12 )) :
+                    obj.logger ( "ANOMALY" , "FAMILY" , "US10" , fam.l_num [ "CHIL" ] [ 0 ] [ 1 ] , fam.fam_id ,
+                                  f"Child with id {c.indi_id} born {c.dob.strftime ( GedcomParser.dateFormatter )} after father's death later than 9 months. Father's death date: {husband.death_date.strftime ( GedcomParser.dateFormatter )}" )
+                    debugger.append ( c.indi_id )
+
+    if debug :
+        return debugger    
+    
